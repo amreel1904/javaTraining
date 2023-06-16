e3a251bc2cbc911e2a0c6d2885c532cc5ff43775
Amreel Nuqman <82134042+amreel1904@users.noreply.github.com>
2023-06-16 15:50:58 +0800

    commit day4

---
+++ src/day4/iterateChar.java
@@ -0,0 +1,11 @@
+package day4;
+
+public class iterateChar {
+    public static void main(String[] args) {
+    for (char ch='a'; ch<= 'z'; ch++){
+        System.out.print(ch+" ");
+    }
+    
+    }
+    
+}
