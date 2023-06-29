843b1a4557b0ca0a2ea917e36d8fdfbde55ad224
Amreel Nuqman <82134042+amreel1904@users.noreply.github.com>
2023-06-29 21:26:14 +0800

    practice

---
+++ src/system/Practice.java
@@ -0,0 +1,12 @@
+package system;
+
+public class Practice {
+	public static void main(String[] args) {
+		int temp = 1;
+		if (temp ==2) {
+			System.out.println(temp);
+		}
+		else
+			System.out.println(temp);
+	}
+}
